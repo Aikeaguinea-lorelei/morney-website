a621b33971ef7dedda7cd4d9e12b9e2debade48f
Aikeaguinea-Lorelei <2656611469@qq.com>
2022-08-07 21:28:13 +0800

    deploy

---
+++ precache-manifest.3c8763a1390d5c66a82249c412f1daa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4409fda7a1c2ba3489",
+    "url": "/morney-website/css/app.6ea36b80.css"
+  },
+  {
+    "revision": "75058f248b07147cbece724bd84b78b4",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "ce4409fda7a1c2ba3489",
+    "url": "/morney-website/js/app.06dadf33.js"
+  },
+  {
+    "revision": "2ee8867b2bb0d584758b",
+    "url": "/morney-website/js/chunk-vendors.52a13a41.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
